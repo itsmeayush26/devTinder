115c1c74aeb96c8aec1cab66a037f7c64df3f768
Ayush Singh <ayushsingh02552gmail.com>
2024-12-07 14:06:36 +0530

    multiple route handlers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,48 @@
 const express= require('express');
 const app = express();
 
+
+//lecture  ->>05
+//one route can have multiple route handler lecture->>05
+
+// first one response!! will be output as js is single threaded language 
+app.use("/user",(req,res,next) =>{
+    console.log("handling the route user!!");
+    // res.send("response!!");
+    next(); ///  ->> it calls the next req,response
+    // res.send("response!!");
+},
+
+
+
+(req,res,next) =>{  ///without adding next here it will not work next(),
+console.log("handling the route user 2!!");
+next();
+res.send("2nd response!!");
+
+},
+// the 3rd and 4th will never be called and taken as output, as next() is calling 2nd response !!
+(req,res,next) =>{
+    console.log("handling the route user 3!!");
+    //res.send("3rd response!!");
+    next();
+    },
+
+    
+
+    (req,res) =>{
+    console.log("handling the route user 4!!");
+   // next(),     //it will through error as nothing on next 
+    res.send("4th response!!");
+
+});
+
+//app,use("/route",[rh1,  rh2, rh3], rh4, rh5)  ///you can pass in btw array also in pllace of first brac() 
+
+
+
+
+
 //because of order post,get and delete will return hehehehhe as a output .......u can ren and test it 
 
 //  app.use("/user", (req,res) => {
@@ -15,7 +57,7 @@ const app = express();
 //     res.send ({firstName:"Ayush ", lastName:"Singh"})
 // });
 
-//this will only handle get call to /user -> lecture 4 HTTPS topic 
+/*//this will only handle get call to /user -> lecture 4 HTTPS topic 
 app.get("/user/:userId/:name/:password", (req,res) => {       //<-params
     console.log (req.params);
     res.send ({firstName:"Ayush ", lastName:"Singh"})
@@ -56,6 +98,9 @@ app.use("/ayush",(req,res) =>{
 app.use("/test",(req,res) =>{ 
     res.send ("hello from the server ");
 });
+*/
+
+
 app.listen(3000, () =>{
     console.log("server is successfully listenning on port 3000...");
 });
